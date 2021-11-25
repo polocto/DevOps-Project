365835559ab1cdad52f6e233a9c3c1b828f045b7
Paul SADE <paul.sade@live.fr>
2021-11-25 02:19:11 +0100

    retry if connection can't be established

--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -5,8 +5,22 @@ const config = configure();
 var client = redis.createClient({
   host: process.env.REDIS_HOST || config.redis.host,
   port: process.env.REDIS_PORT || config.redis.port,
-  retry_strategy: () => {
-    return new Error("Retry time exhausted");
+  retry_strategy: (options) => {
+    if (options.error && (options.error.code === 'ECONNREFUSED' || options.error.code === 'NR_CLOSED')) {
+        // Try reconnecting after 5 seconds
+        console.error('The server refused the connection. Retrying connection...');
+        return 5000;
+    }
+    if (options.total_retry_time > 1000 * 60 * 60) {
+        // End reconnecting after a specific timeout and flush all commands with an individual error
+        return new Error('Retry time exhausted');
+    }
+    if (options.attempt > 50) {
+        // End reconnecting with built in error
+        return undefined;
+    }
+    // reconnect after
+    return Math.min(options.attempt * 100, 3000);
   }
 });
 
